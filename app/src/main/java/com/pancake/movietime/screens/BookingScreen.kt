e80f34a383dd2874f7dcdd2397d751fd3a031fae
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-04 11:05:47 +0300

    create BookingScreen

--- app/src/main/java/com/pancake/movietime/screens/BookingScreen.kt
+++ app/src/main/java/com/pancake/movietime/screens/BookingScreen.kt
@@ -3,12 +3,10 @@ package com.pancake.movietime.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.pancake.movietime.composable.BottomCard
 
 @Composable
