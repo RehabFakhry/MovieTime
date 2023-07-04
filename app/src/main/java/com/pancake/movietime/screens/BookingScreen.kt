d38d29055072bc0f8f614aab1ba76d9ac9e7794a
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-04 11:01:40 +0300

    create BookingScreen

--- app/src/main/java/com/pancake/movietime/screens/BookingScreen.kt
+++ app/src/main/java/com/pancake/movietime/screens/BookingScreen.kt
@@ -1,8 +1,30 @@
 package com.pancake.movietime.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pancake.movietime.composable.BottomCard
 
 @Composable
 fun BookingScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Spacer(modifier = Modifier.weight(1f))
+        BottomCard()
+    }
+}
 
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewBookingScreen(){
+    BookingScreen()
 }
