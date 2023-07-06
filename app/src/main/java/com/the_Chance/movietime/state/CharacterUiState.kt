6f633373ab9cac9c0389ba68541a558052919059
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-07-06 10:02:44 +0300

    add viewModel and lazyRows

--- app/src/main/java/com/the_Chance/movietime/state/CharacterUiState.kt
+++ app/src/main/java/com/the_Chance/movietime/state/CharacterUiState.kt
@@ -2,10 +2,10 @@ package com.the_Chance.movietime.state
 
 
 data class CharactersUiState(
-    val character: List<CharacterUiState> = emptyList()
+    val characters: List<CharacterUiState> = emptyList()
 )
 
 data class CharacterUiState(
     val name: String,
-    val imageUrl: String,
+    val image: Int,
 )
--- app/src/main/java/com/the_Chance/movietime/viewModel/CharacterViewModel.kt
+++ app/src/main/java/com/the_Chance/movietime/viewModel/CharacterViewModel.kt
@@ -1,6 +1,7 @@
 package com.the_Chance.movietime.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.the_Chance.movietime.R
 import com.the_Chance.movietime.state.CharacterUiState
 import com.the_Chance.movietime.state.CharactersUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,26 +9,26 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-//@HiltViewModel
-//class CharacterViewModel: ViewModel() {
-//    private val _state = MutableStateFlow(CharactersUiState())
-//    val state = _state.asStateFlow()
-//
-//    init {
-//        getAllCharacters()
-//    }
-//
-//    private fun getAllCharacters(){
-//        _state.update { it.copy(
-//            character = listOf(
-//                CharacterUiState("Harry Potter", "https://coppellstudentmedia.com/wp-content/uploads/2012/01/Daniel-Radcliffe-900x616.jpg"),
-//                CharacterUiState("Emma Watson",  "https://api.time.com/wp-content/uploads/2014/07/emma-watson.jpg"),
-//                CharacterUiState("RON BILIUS WEASLEY", "https://i.quotev.com/cb6x6mhnaaaa.jpg"),
-//                CharacterUiState("Tom Cruise", "https://flxt.tmsimg.com/assets/378_v9_be.jpg"),
-//                CharacterUiState("Tom Hanks", "https://people.com/thmb/VhY3kcy268Af3D1JPS4BilhLYGI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/tom-hanks-pre-grammy-gala-051023-1-ab2497f7246443a4824efe90dda4ee40.jpg"),
-//                CharacterUiState("Angelina Jolie ", "https://assets.ayobandung.com/crop/56x366:719x956/750x500/webp/photo/2023/03/26/Angelina-Jolie-3647229494.png"),
-//                CharacterUiState("Cat", "https://cdn-prd.content.metamorphosis.com/wp-content/uploads/sites/6/2022/12/shutterstock_781327003-1.jpg")
-//            )
-//        ) }
-//    }
-//}
+@HiltViewModel
+class CharacterViewModel: ViewModel() {
+    private val _state = MutableStateFlow(CharactersUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        getAllCharacters()
+    }
+
+    private fun getAllCharacters(){
+        _state.update { it.copy(
+            characters = listOf(
+                CharacterUiState("Harry Potter", R.drawable.harry_potter),
+                CharacterUiState("Emma Watson",  R.drawable.emma_watson),
+                CharacterUiState("RON BILIUS WEASLEY", R.drawable.ron_bilius),
+                CharacterUiState("Tom Cruise", R.drawable.tom_cruise),
+                CharacterUiState("Tom Hanks", R.drawable.tom_hanks),
+                CharacterUiState("Angelina Jolie ", R.drawable.anglina_jolie),
+                CharacterUiState("Cat", R.drawable.cat)
+            )
+        ) }
+    }
+}
